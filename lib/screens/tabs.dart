0823002bf3e2b82451d14aba0d2cf34aba160fc2
Jonathan <dos.santos.2k23@gmail.com>
2025-07-03 07:29:21 -0300

    Feature: Favorite Meals state management

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -13,16 +13,29 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
 
-  final List<Meal> _favoriteMeals= [];
+  final List<Meal> _favoriteMeals = [];
+
+    void _showInfoMessage(String message) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
+      Text(message)
+      ));
+    }
 
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
 
     if (isExisting) {
-      _favoriteMeals.remove(meal);
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage('Meal removed from favorites');
     } else {
-      _favoriteMeals.add(meal);
-  }
+      setState(() {
+        _favoriteMeals.add(meal);
+      });
+      _showInfoMessage('Marked as a favorite!');
+    }
   }
 
   void _selectPage(int index) {
@@ -33,26 +46,32 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    Widget activePage = CategoriesScreen(onToggleFavorite: _toggleMealFavoriteStatus);
+    Widget activePage = CategoriesScreen(
+      onToggleFavorite: _toggleMealFavoriteStatus,
+    );
     var activePageTitle = 'Categories';
 
     if (_selectedPageIndex == 1) {
-      activePage = MealsScreen(availableMeals: const [], onToggleFavorite: _toggleMealFavoriteStatus);
+      activePage = MealsScreen(
+        availableMeals: _favoriteMeals,
+        onToggleFavorite: _toggleMealFavoriteStatus,
+      );
       activePageTitle = 'Your Favorites';
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(activePageTitle),
-      ),
+      appBar: AppBar(title: Text(activePageTitle)),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
-          onTap: _selectPage,
-          currentIndex: _selectedPageIndex,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.set_meal), label: 'Categories'),
-            BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites')
-          ]
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites'),
+        ],
       ),
     );
   }
