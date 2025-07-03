6fe835467345145b29633e38a0f9df1f347bbfe9
Jonathan <dos.santos.2k23@gmail.com>
2025-07-03 17:28:43 -0300

    Feature: Returning data when leaving FiltersScreen, by use of PopScope

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -4,6 +4,13 @@ import 'package:meals_app/widgets/switch_list_tile_custom.dart';
 // import 'package:meals_app/screens/tabs.dart';
 // import 'package:meals_app/widgets/drawer.dart';
 
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
 
@@ -32,48 +39,61 @@ class _FiltersScreenState extends State<FiltersScreen> {
       //     }
       //   },
       // ),
-      body: Column(
-        children: [
-          SwitchListTileCustom(
-            value: _glutenFreeFilterSet,
-            title: 'Gluten-free',
-            subtitle: 'Only include gluten-free meals.',
-            onChanged: (isChecked) {
-              setState(() {
-                _glutenFreeFilterSet = isChecked;
-              });
-            },
-          ),SwitchListTileCustom(
-            value: _lactoseFreeFilterSet,
-            title: 'Lactose-free',
-            subtitle: 'Only include lactose-free meals.',
-            onChanged: (isChecked) {
-              setState(() {
-                _lactoseFreeFilterSet = isChecked;
-              });
-            },
-          ),
-          SwitchListTileCustom(
-            value: _vegetarianFilterSet,
-            title: 'Vegetarian',
-            subtitle: 'Only include vegetarian meals.',
-            onChanged: (isChecked) {
-              setState(() {
-                _vegetarianFilterSet = isChecked;
-              });
-            },
-          ),
-          SwitchListTileCustom(
-            value: _veganFilterSet,
-            title: 'Vegan',
-            subtitle: 'Only include vegan meals.',
-            onChanged: (isChecked) {
-              setState(() {
-                _veganFilterSet = isChecked;
-              });
-            },
-          ),
-        ],
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (bool dipPop, dynamic result) {
+          if(dipPop) return;
+          Navigator.of(context).pop({
+            Filter.glutenFree: _glutenFreeFilterSet,
+            Filter.lactoseFree: _lactoseFreeFilterSet,
+            Filter.vegetarian: _vegetarianFilterSet,
+            Filter.vegan: _veganFilterSet,
+          });
+        },
+        child: Column(
+          children: [
+            SwitchListTileCustom(
+              value: _glutenFreeFilterSet,
+              title: 'Gluten-free',
+              subtitle: 'Only include gluten-free meals.',
+              onChanged: (isChecked) {
+                setState(() {
+                  _glutenFreeFilterSet = isChecked;
+                });
+              },
+            ),
+            SwitchListTileCustom(
+              value: _lactoseFreeFilterSet,
+              title: 'Lactose-free',
+              subtitle: 'Only include lactose-free meals.',
+              onChanged: (isChecked) {
+                setState(() {
+                  _lactoseFreeFilterSet = isChecked;
+                });
+              },
+            ),
+            SwitchListTileCustom(
+              value: _vegetarianFilterSet,
+              title: 'Vegetarian',
+              subtitle: 'Only include vegetarian meals.',
+              onChanged: (isChecked) {
+                setState(() {
+                  _vegetarianFilterSet = isChecked;
+                });
+              },
+            ),
+            SwitchListTileCustom(
+              value: _veganFilterSet,
+              title: 'Vegan',
+              subtitle: 'Only include vegan meals.',
+              onChanged: (isChecked) {
+                setState(() {
+                  _veganFilterSet = isChecked;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
