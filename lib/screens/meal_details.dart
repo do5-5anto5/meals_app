2f9ebe9aa7caf398f5392b1ee9cc7cb43b73d452
Jonathan <dos.santos.2k23@gmail.com>
2025-07-05 16:54:04 -0300

    feat: Implicit transition at MealDetailsScreen's favorite button

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -28,12 +28,25 @@ class MealDetailsScreen extends ConsumerWidget {
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: Text(
-                    wasAdded ? 'Meal added as a favorite' : 'Meal removed from favorites.',
+                    wasAdded
+                        ? 'Meal added as a favorite'
+                        : 'Meal removed from favorites.',
                   ),
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder:
+                  (child, animation) => RotationTransition(
+                    turns: Tween<double>(begin: 0.8, end: 1.0).animate(animation),
+                    child: child,
+                  ),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           ),
         ],
       ),
